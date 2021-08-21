d541cb7a5bf1e980b1f4f2b48dd69302ee8cb17b
quanganh14 <89283776+quanganh14@users.noreply.github.com>
2021-08-21 10:24:55 +0700

    Add files via upload

---
+++ blockdev-nft.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.8.3;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract BlockdevNFT is ERC721 {
+    using Strings for uint256;
+    string public baseURI = "https://raw.githubusercontent.com/quanganh14/NFT/main/cards/";
+
+    constructor() ERC721("NFT Demo", "NFTD") {
+
+    }
+
+    // https://raw.githubusercontent.com/quanganh14/NFT/main/cards/CUONGTHI.json
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory buri = baseURI;
+        return bytes(buri).length > 0
+            ? string(abi.encodePacked(buri, tokenId.toString(), ".json"))
+            : '';
+    }
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+}
